4995504e4fff2bfdc32054fbe813d4bafd4a0ae7
MerlotRain <122578849+MerlotRain@users.noreply.github.com>
2023-12-15 13:43:50 +0800

    Update geohash.c

--- mathSE/index/geohash.c
+++ mathSE/index/geohash.c
@@ -1,1 +1,22 @@
+#include "geohash.h"
+
+#define GEO_LAT_MIN -85.05112878
+#define GEO_LAT_MAX 85.05112878
+#define GEO_LONG_MIN -180
+#define GEO_LONG_MAX 180
+
+struct geo_point {
+  double longitude;
+  double latitude;
+  double code;
+  char *name;
+};
+
+struct geo_array {
+  struct geo_point *array;
+  size_t buckets;
+  size_t used;
+};
+
+struct geo_array *g_geo_array = NULL;
 
