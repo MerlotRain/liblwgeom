1161be936bea664e24d86daebc391c676487d170
MerlotRain <122578849+MerlotRain@users.noreply.github.com>
2024-01-15 16:01:58 +0800

    Update coordinate.h

--- mathSE/coordinate.h
+++ mathSE/coordinate.h
@@ -7,6 +7,23 @@
 extern "C" {
 #endif
 
+#define INTERPRET_TYPE_POINT 1
+#define INTERPRET_TYPE_LINE 2
+#define INTERPRET_TYPE_POLYGON_EXTERIOR 1003
+#define INTERPRET_TYPE_POLYGON_INTERIOR 2003
+#define INTERPRET_TYPE_COMPOUND_LINE 4
+#define INTERPRET_TYPE_COMPOUND_POLYGON_EXTERIOR 1005
+#define INTERPRET_TYPE_COMPOUND_POLYGON_INTERIOR 2005
+
+#define INTERPRET_PRE_SINGLE_POINT 1
+#define INTERPRET_PRE_LINE_PATH 1
+#define INTERPRET_PRE_LINE_ARC 2
+#define INTERPRET_PRE_LINE_B_SHAPE 3
+#define INTERPRET_PRE_POLYGON_PATH 1
+#define INTERPRET_PRE_POLYGON_ARC 2
+#define INTERPRET_PRE_POLYGON_RECTANGLE 3
+#define INTERPRET_PRE_POLYGON_CIRCLE 4
+
 /* The geometry memory model is a simplified SDO_Geometry model. */
 typedef struct se_coordinate_blob {
   uint8_t geo_type;
@@ -23,6 +40,10 @@ typedef struct se_interpret_tuple {
   int Type;   // element type
   int Pre;    // interpretation
 } interpret_tuple_t;
+
+#define INT_TUP_POINT                                                          \
+  interpret_tuple_t __itt__ = {                                                \
+      .Offset = 1, .Type = INTERPRET_TYPE_POINT, .Pre = 1};
 
 #ifdef __cplusplus
 }
