a89fcdbacb6132f46555fec23329d9bb23164692
MerlotRain <122578849+MerlotRain@users.noreply.github.com>
2024-07-15 20:09:14 +0800

    update algorithm

--- src/mathse_geom.h
+++ src/mathse_geom.h
@@ -1,10 +1,11 @@
 #ifndef __MATHSE_GEOM_H__
 #define __MATHSE_GEOM_H__
 
+#include <mathse_export.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 typedef enum {
     /* Invalid geometry type */
@@ -40,6 +41,18 @@ typedef enum {
     /* Dimension value of area geometry (2) */
     A = 2,
 } GeometryDimension;
+
+
+/* Segmentation tolerance as maximum angle or maximum difference between
+ * approximation and circle */
+typedef enum {
+    /* Maximum angle between generating radii (lines from arc center to output
+    * vertices) */
+    MAX_ANGLE,
+    /* Maximum distance between an arbitrary point on the original curve and closest
+    * point on its approximation. */
+    MAX_DIFFERENCE
+} SegmentToleranceType;
 
 /* --------------------------- Simple OGRGeometry --------------------------- */
 
@@ -152,9 +165,43 @@ typedef struct geom_coordinate_t {
         geom_mlinestring_t *mlinestring;
         geom_mpolygon_t    *mpolygon;
     } oti;
-    int geom_dim;
 } geom_coordinate_t;
 
+
+/* --------------------------- Coordinate Factory --------------------------- */
+
+/* create point. */
+SE_API geom_coordinate_t *create_point(const double *p);
+
+
+/* convert geom_lint_t to geom_coordinate_t. */
+SE_API geom_coordinate_t *convert_line_to_coordinate(const geom_line_t *p);
+
+SE_API geom_coordinate_t *convert_arc_to_coordinate(const geom_arc_t *p);
+
+SE_API geom_coordinate_t *convert_envelope_to_coordinate(const geom_envelope_t* p);
+
+SE_API geom_coordinate_t *convert_ellipse_to_coordinate(const geom_ellipse_t* p, bool fill);
+
+SE_API geom_coordinate_t *convert_triangle_to_coordinate(const geom_triangle_t* p);
+
+/* create arc line by three point */
+SE_API geom_coordinate_t *create_arc1(const double *p);
+
+/* create arc by circle and sweep angle */
+SE_API geom_coordinate_t *create_arc2(const double *c,
+                                      const double  radius,
+                                      const double  angle1,
+                                      const double  angle2);
+
+/* create arc by begin and end point, chord */
+SE_API geom_coordinate_t *create_arc3(const double *p, const double chord);
+
+/* create single path */
+SE_API geom_coordinate_t *create_linestring(const double *p, const int num);
+
+/* create envelope */
+SE_API geom_coordinate_t *create_envelope(const double *p);
 
 #ifdef __cplusplus
 }
